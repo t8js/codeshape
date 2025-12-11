0498d46c1cf932c29a1c48c664efbbe055ecea8f
axtk <axtkx@inbox.ru>
2025-12-11 21:10:44 +0300

    lint

--- src/compile.ts
+++ src/compile.ts
@@ -1,10 +1,10 @@
 import { readFile, rename, unlink, writeFile } from "node:fs/promises";
+import { join } from "node:path";
 import { formatDuration } from "@t8/date-format";
 import { exec } from "./utils/exec.ts";
 import { getArgValue } from "./utils/getArgValue.ts";
 import { getArgValues } from "./utils/getArgValues.ts";
 import { log } from "./utils/log.ts";
-import { join } from "node:path";
 
 const { argv } = process;
 
@@ -43,7 +43,7 @@ export async function compile() {
   } catch {}
 
   await Promise.all(
-    ["index.cjs", "index.mjs", "index.d.ts"].map(async name => {
+    ["index.cjs", "index.mjs", "index.d.ts"].map(async (name) => {
       try {
         let path = join(output, name);
         let s = (await readFile(path)).toString();
@@ -52,7 +52,7 @@ export async function compile() {
           .replace(/^\/\/#(region \S+|endregion)$/gm, "")
           .replace(/\r\n/g, "\n")
           .replace(/\n{3,}/g, "\n\n")
-          .replace(/^\t+/gm, t => t.replaceAll("\t", "  "))
+          .replace(/^\t+/gm, (t) => t.replaceAll("\t", "  "))
           .trim();
 
         await writeFile(path, `${s}\n`);
